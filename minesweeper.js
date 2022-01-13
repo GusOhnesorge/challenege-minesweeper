3d0b33c6c67dc6370a042a0fd263433d6e5a2c1b
Gus <ohnesorge.gus@gmail.com>
2022-01-12 18:30:01 -0600

    convert dict references to array incrementing

--- minesweeper.js
+++ minesweeper.js
@@ -4,69 +4,64 @@
 //
 
 export const annotate = (input) => {
-  let coords = {}
   // parse rows
   for (let row = 0; row<input.length(); row++){
     // convert rows from strings to individual chars in array
-    let split_row = input[row].split()
-    for (let col = 0; col<split_row.length(); col++){
+    input[row] = input[row].split()
+  }
+
+  for (let row = 0; row<input.length(); row++){
+    for (let col = 0; col<input[row].length(); col++){
       if(split_row[col] === '*'){
-        coords = increment_coords_from_bomb(row, col, row.length(), col.length(), coords)
+        input = increment_coords_from_bomb(row, col, input)
       }
     }
-  }
+
   
 };
 
-function increment_coords_from_bomb(b_row, b_col, max_row, max_col, coords){
+function increment_coords_from_bomb(cur_row, cur_col, input){
   // unclear if using space to store these checks is worth any tiny computational time gain
   let neg_col = false
   let pos_col = false
-  let point = b_row + "," + b_col
-  coords[point] = -1
-  function increment(cur_point) {
-    if (coords[cur_point] === undefined){
-      coords[cur_point] = 1
+  input[cur_row][cur_col] = '*'
+  function increment(dif_row, dif_col) {
+    if (input[dif_row][dif_col] === " "){
+      input[dif_row][dif_col] = 1
     }
-    else if (coords[cur_point] >= 1){
-      coords[cur_point] = coords[cur_point]+=1
+    // '*' is not >= than 1
+    else if (input[dif_row][dif_col] >= 1){
+      input[dif_row][dif_col] = input[dif_row][dif_col]+=1
     }
   }
   // incrementing same row as bomb
   if(b_col-1>-1){
     neg_col = true
-    point = b_row + "," + (b_col-1)
-    increment(point)
+    increment(cur_row, cur_col-1)
   }
   if(b_col+1<max_col){
     pos_col = true
-    point = b_row + "," + (b_col+1)
-    increment(point)
+    increment(cur_row, cur_col+1)
   }
   // incrementing row "above" bomb if possible
   if(b_row-1>-1){
     if(neg_col){
-      point = (b_row-1) + "," + (b_col-1)
-      increment(point)
+      increment(cur_row-1, cur_col-1)
     }
     if(pos_col){
-      point = (b_row-1) + "," + (b_col+1)
-      increment(point)
+      increment(cur_row-1, cur_col+1)
     }
     point = (b_row-1) + "," + (b_col)
-    increment(point)
+    increment(cur_row-1, cur_col)
   }
   // incrementing row "below" bomb if possible
   if(b_row+1<max_row){
     if(neg_col){
-      point = (b_row+1) + "," + (b_col-1)
-      increment(point)
+      increment(cur_row+1, cur_col-1)
     }
     if(pos_col){
-      point = (b_row+1) + "," + (b_col+1)
-      increment(point)
+      increment(cur_row+1, cur_col+1)
     }
-    point = (b_row+1) + "," + (b_col)
-    increment(point)
+    increment(cur_row+1, cur_col)
   }
 }
