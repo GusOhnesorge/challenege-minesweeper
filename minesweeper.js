c1dbd01a58bb52f649c37c41ef706a54ec149811
Gus <ohnesorge.gus@gmail.com>
2022-01-12 19:49:16 -0600

    fixed increment function not returning anything

--- minesweeper.js
+++ minesweeper.js
@@ -5,31 +5,30 @@
 
 export const annotate = (input) => {
   // parse rows
-  for (let row = 0; row<input.length(); row++){
+  for (let row = 0; row<input.length; row++){
     // convert rows from strings to individual chars in array
-    input[row] = input[row].split()
+    input[row] = input[row].split("")
   }
-
-  for (let row = 0; row<input.length(); row++){
-    for (let col = 0; col<input[row].length(); col++){
-      if(split_row[col] === '*'){
+  // find bombs and increment around them
+  for(let row = 0; row<input.length; row++){
+    for(let col = 0; col<input[row].length; col++){
+      if(input[row][col] === '*'){
         input = increment_coords_from_bomb(row, col, input)
       }
     }
-
-    // collapse all of the columns back into strings
-    for (let row = 0; row<input.length(); row++){
-      input[row] = input[row].join()
-    }
+  }
+  // collapse all of the columns back into strings
+  for (let row = 0; row<input.length; row++){
+    input[row] = input[row].join('')
+  }
 
   return input
-};
+}
 
 function increment_coords_from_bomb(cur_row, cur_col, input){
   // unclear if using space to store these checks is worth any tiny computational time gain
   let neg_col = false
   let pos_col = false
-  input[cur_row][cur_col] = '*'
   function increment(dif_row, dif_col) {
     if (input[dif_row][dif_col] === " "){
       input[dif_row][dif_col] = 1
@@ -40,27 +39,26 @@ function increment_coords_from_bomb(cur_row, cur_col, input){
     }
   }
   // incrementing same row as bomb
-  if(b_col-1>-1){
+  if(cur_col-1>-1){
     neg_col = true
     increment(cur_row, cur_col-1)
   }
-  if(b_col+1<max_col){
+  if(cur_col+1<input[cur_row].length){
     pos_col = true
     increment(cur_row, cur_col+1)
   }
   // incrementing row "above" bomb if possible
-  if(b_row-1>-1){
+  if(cur_row-1>-1){
     if(neg_col){
       increment(cur_row-1, cur_col-1)
     }
     if(pos_col){
       increment(cur_row-1, cur_col+1)
     }
-    point = (b_row-1) + "," + (b_col)
     increment(cur_row-1, cur_col)
   }
   // incrementing row "below" bomb if possible
-  if(b_row+1<max_row){
+  if(cur_row+1<input.length){
     if(neg_col){
       increment(cur_row+1, cur_col-1)
     }
@@ -69,4 +67,5 @@ function increment_coords_from_bomb(cur_row, cur_col, input){
     }
     increment(cur_row+1, cur_col)
   }
+  return input
 }
